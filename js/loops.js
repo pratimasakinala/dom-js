d78130cca85c87ad4823f31df99573a5057fb28e
Pratima Sakinala <pratimasakinala13@gmail.com>
2019-04-29 20:58:00 -0700

    learning about for, do while

---
+++ js/loops.js
@@ -0,0 +1,22 @@
+// array of fruits
+// add 5 fruits in the array
+var fruits = ['apple', 'banana', 'orange', 'kiwi', 'pineapple', 'strawberry'];
+console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+// console.log(fruits[4]);
+
+// for loop
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log(i);
+//     console.log(fruits[i]);
+// }
+
+// do while
+var index = 0;
+do {
+    console.log(fruits[index]);
+    index++;
+} while (index < fruits.length);
