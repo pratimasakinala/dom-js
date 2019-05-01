67cc66d8e867c4658927055786cf5a0695f1a589
Pratima Sakinala <pratimasakinala13@gmail.com>
2019-04-30 19:33:11 -0700

    create operator script file

---
+++ js/operators.js
@@ -0,0 +1,7 @@
+// Assignment:
+// 2 variables x & y that store numbers & assign some values
+// check if x is greater than 18 & y is greater than 21
+// then display "Horray!"
+// else if x is greater than 14 & less than 18
+    // then check if y is greater than 18 & less than 21, display "There is time!"
+    // else display "You are late!"
