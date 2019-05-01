062b469fc8f248b65d3dbb1aa86a29576fa2b41d
Pratima Sakinala <pratimasakinala13@gmail.com>
2019-04-30 18:47:53 -0700

    learning while, for in & for of loops

--- js/loops.js
+++ js/loops.js
@@ -16,7 +16,25 @@ console.log(fruits);
 
 // do while
 var index = 0;
-do {
-    console.log(fruits[index]);
-    index++;
-} while (index < fruits.length);
+// do {
+//     console.log(fruits[index]);
+//     index++;
+// } while (index < fruits.length);
+
+// while
+// while (index < fruits.length) {
+//     console.log(fruits[index]);
+//     // index++;
+//     index += 1;
+// }
+
+// for in
+fruits.foo = 'test';
+for (var i in fruits) {
+    console.log(i);
+}
+
+// for of
+for (var i of fruits) {
+    console.log(i);
+}
